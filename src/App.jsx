e6f2e66b82d6ad7ba10e9a67d049b08defd8dd91
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-01 17:57:10 -0600

    hanldled error with errorElement

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from './ui/Error'
 
 
 const router = createBrowserRouter([
@@ -14,6 +15,9 @@ const router = createBrowserRouter([
     //because AppLayout will be accessible to every page
     //so it is a parent page
     element: <AppLayout />,
+    //we specify error element here because errors happen in childre components 
+    // will bubble up to the parent routes
+    errorElement:<Error />,
     children: [
       {
         path: "/",
@@ -25,6 +29,8 @@ const router = createBrowserRouter([
         //add loader to tell react this is the loader
         // so that it will fetch data when the component starts rendering
         loader: menuLoadr,
+        // we want the error can be shown in menu
+        errorElement: <Error />
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
