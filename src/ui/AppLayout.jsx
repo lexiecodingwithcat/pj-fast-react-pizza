89d0ad503da51a5dbd7cb20d972af51342d44309
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-02 23:52:00 -0600

    styled css grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,18 +7,22 @@ function AppLayout() {
   // we want to have a huge Loader so that when one page of the whole app is loading
   // we show the loader
   const navigation = useNavigation();
-  
+
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
-  
+    // auto means it will take the space based on its content
+    //h-screen will be the 100vh of the viewpoint
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       <Header />
-      <main>
-        {/*  we want the main content changes based on current route */}
-        {/* Outlet is like a placeholdre taht allows browser to render child routes inside its parent route  */}
-        {isLoading ? <Loader /> : <Outlet />}
-      </main>
+      <div className="overflow-scroll ">
+        {/*  we can use margin left and right set to auto to place it in the middle */}
+        <main className="mx-auto max-w-3xl bg-red-800 ">
+          {/*  we want the main content changes based on current route */}
+          {/* Outlet is like a placeholdre taht allows browser to render child routes inside its parent route  */}
+          {isLoading ? <Loader /> : <Outlet />}
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
