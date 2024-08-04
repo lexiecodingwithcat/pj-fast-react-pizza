e9ee69319f93e52206e39f4870d6e9edc478f77f
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-04 15:19:24 -0600

    styled form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -61,7 +61,12 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="focus: w-full rounded-full border border-stone-200 px-4 py-2 text-sm ring-yellow-400 transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring md:px-6 md:py-3"
+            />
           </div>
         </div>
 
@@ -72,6 +77,7 @@ function CreateOrder() {
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,6 +17,7 @@ function SearchOrder() {
         placeholder="search order#"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="transition-all w-28 rounded-full bg-yellow-100 px-4 py-2 text-xs duration-300 placeholder:text-stone-400 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:text-sm"
       />
     </form>
   );
