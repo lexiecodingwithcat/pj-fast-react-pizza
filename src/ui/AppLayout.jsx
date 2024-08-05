a38fc745207ae1989e8735e90475784faeb76c85
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-04 21:34:37 -0600

    applied position

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import {
+  Outlet,
+  useNavigation,
+} from "react-router-dom";
 import Loader from "./Loader";
 
 function AppLayout() {
@@ -8,19 +11,21 @@ function AppLayout() {
   // we show the loader
   const navigation = useNavigation();
 
-  const isLoading = navigation.state === "loading";
+  const isLoading =
+    navigation.state === "loading";
 
   return (
     // auto means it will take the space based on its content
     //h-screen will be the 100vh of the viewpoint
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       <Header />
-      <div className="overflow-scroll ">
+      <div className="overflow-scroll">
         {/*  we can use margin left and right set to auto to place it in the middle */}
         <main className="mx-auto max-w-3xl">
           {/*  we want the main content changes based on current route */}
           {/* Outlet is like a placeholdre taht allows browser to render child routes inside its parent route  */}
           {isLoading ? <Loader /> : <Outlet />}
+          {/* {true && <Loader />} */}
         </main>
       </div>
       <CartOverview />
