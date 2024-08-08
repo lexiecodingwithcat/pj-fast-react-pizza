edd7a8e66ebf9faa8989f1c72083080d5a73edf6
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-08 12:56:07 -0600

    created cart slice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -13,7 +13,7 @@ const initialState = {
   ],
 };
 
-const createSlice = createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducer: {
@@ -22,9 +22,43 @@ const createSlice = createSlice({
       //push is a method used by JS array to push the new item to the end of the array
       state.cart.push(action.payload);
     },
-    deleteItem(state, action) {},
-    increaseItemQuantity(state, action) {},
-    decreaseItemQuantity(state, action) {},
-    clearCart(state, action) {},
+    deleteItem(state, action) {
+      //payload= id
+      //assign new array to the state.cart
+      state.cart = state.cart.filter(
+        (item) => item.pizzaId !== action.payload,
+      );
+    },
+    increaseItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find(
+        (item) => item.pizzaId === action.payload,
+      );
+      item.qunatity++;
+      //update the total price as well
+      item.totalPrice =
+        item.qunatity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find(
+        (item) => item.pizzaId === action.payload,
+      );
+      item.qunatity--;
+      item.totalPrice =
+        item.qunatity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
   },
 });
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
