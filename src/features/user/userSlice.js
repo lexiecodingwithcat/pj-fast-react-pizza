cf48831f601b7c61d16e373352da92886353a46f
Tianzi Cui <cuitianzi0@gmail.com>
2024-08-10 22:19:31 -0600

    created async Thunk and connected it to userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,29 +1,47 @@
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+import {
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
 
-import { createSlice } from "@reduxjs/toolkit";
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(
+      resolve,
+      reject,
+    );
+  });
+}
 
-// async function fetchAddress() {
-//   // 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
+//1.create the Thunk
+//params: 1. pass the action name; 2. pass an asyn function that will return payload for reducer
+//do not call it getAddress cuz that is reserved for useSelector
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+    //   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-//   // 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
+    //   // 3) Then we return an object with the data that we are interested in
+    //payload of the fullfilled state
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: " ",
+  status: "idel",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -34,6 +52,30 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  //2. connect the reducer with Thunk and response the lifecycle of async process ( pending, fullfilled and rejected)
+  extraReducers: (builder) =>
+    builder
+      .addCase(
+        fetchAddress.pending,
+        (state) => (state.status = "loading"),
+      )
+      .addCase(
+        fetchAddress.fulfilled,
+        (state, action) => {
+          state.position =
+            action.payload.position;
+          state.address = action.payload.address;
+          state.status = "idel";
+        },
+      )
+      //if the user does not accept Geolocation
+      .addCase(
+        fetchAddress.rejected,
+        (state, action) => {
+          state.status = "error";
+          state.error = action.error.message;
+        },
+      ),
 });
 
 export default userSlice.reducer;
